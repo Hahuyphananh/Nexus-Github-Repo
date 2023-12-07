4f8027789ac8b2073e545cf3c5cf6a70aa2b0d8c
Hahuyphananh <phananhalbert@gmail.com>
2023-12-07 09:17:32 -0500

    drag n drop ui and delete main.css, html templates

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("list", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("list");
+    ev.target.appendChild(document.getElementById(data));
+    console.log(1);
+}
