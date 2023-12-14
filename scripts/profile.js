ed31000ac8666e7cd3d8eaa237a78c5f1cce51bd
Hahuyphananh <phananhalbert@gmail.com>
2023-12-14 09:54:48 -0500

    added edit profile overlay box, javascript changed for image upload

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,30 @@
 let profilePic = document.getElementById("profile-pic");
 let inputFile = document.getElementById("input-file");
 
-inputFile.onchange = function () {
-    profilePic.src = URL.createObjectURL(inputFile.files[0]);
+/*function pfpChange() {
+    inputFile.onchange = function () {
+        profilePic.src = URL.createObjectURL(inputFile.files[0]);
+    }
+    document.inputFile.style.display = "block";
+}*/
+
+function changeImage() {
+    var userImage = document.getElementById("input-file").value;
+    document.getElementById("profile-pic").src = userImage;
+}
+
+/*function pfpChange() {
+    document.getElementById("swim-image").style.display = "block";
+    document.getElementById("swim-image").style.zIndex = "10";
+}*/
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("edit-profile").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("edit-profile").style.display = "none";
+
 }
